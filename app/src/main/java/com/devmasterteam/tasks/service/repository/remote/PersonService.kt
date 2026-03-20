c1174702e97449069bbf8075d9d885bc1fe036d6
Eric Bortoleto <ericbortoleto_9@outlook.com>
2026-03-20 18:06:22 -0300

    Define PersonModel and implement PersonService with login and create endpoints.

---
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/remote/PersonService.kt
@@ -0,0 +1,33 @@
+package com.devmasterteam.tasks.service.repository.remote
+
+import com.devmasterteam.tasks.service.model.PersonModel
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface PersonService {
+    // Implementação da chamada de login
+
+    @POST("Authentication/Login")
+    @FormUrlEncoded
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Response<PersonModel> {
+
+        return TODO("Provide the return value")
+    }
+
+    @POST("Authentication/Create")
+    @FormUrlEncoded
+    fun create(
+        @Field("email") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("password") receivenews: String
+    ): Response<PersonModel> {
+
+        return TODO("Provide the return value")
+    }
+}
