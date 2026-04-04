3815274baa9c5147c5719c6bfaa7a7b3675b98f7
Bortoleto <ericbortoleto_9@outlook.com>
2026-04-04 15:20:37 -0300

    Implement user name in MainViewModel

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/MainViewModel.kt
@@ -3,6 +3,8 @@ package com.devmasterteam.tasks.viewmodel
 import android.app.Application
 import android.app.TaskStackBuilder
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.repository.local.PreferencesManager
@@ -11,6 +13,9 @@ import kotlinx.coroutines.launch
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val preferencesManager = PreferencesManager(application.applicationContext)
 
+    private val _name = MutableLiveData<String>()
+    val name: LiveData<String> = _name
+
     fun logout() {
         viewModelScope.launch {
             preferencesManager.remove(TaskConstants.SHARED.TOKEN_KEY)
@@ -18,5 +23,11 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             preferencesManager.remove(TaskConstants.SHARED.PERSON_NAME)
         }
 
+    }
+
+    fun loadUserName() {
+        viewModelScope.launch {
+            _name.value = preferencesManager.get(TaskConstants.SHARED.PERSON_NAME)
+        }
     }
 }
